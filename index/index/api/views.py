76f17bc704413b9aef4f941a2bcba6907462f196
danclee336 <danclee@umich.edu>
2020-12-03 03:04:21 -0500

    Sorting score left

--- index/index/api/views.py
+++ index/index/api/views.py
@@ -2,15 +2,18 @@
 import flask
 import index
 import re
+import numpy as np
+from collections import Counter
 
 index_package_dir = pathlib.Path(__file__).parent.parent
+
 stopwords_filename = index_package_dir/"stopwords.txt"
 pagerank_filename = index_package_dir/"pagerank.out"
 inverted_index_filename = index_package_dir/"inverted_index.txt"
 
 stopwords = get_stopwords()
 pagerank = get_pagerank()
-inverted_index = get_inverted_index()
+inverted_index = get_inverted_index() #{term: [idf, {doc_id: [freq in doc, squared norm factor]}]}
 
 @index.app.route("/api/v1/", methods=["GET"])
 def get_dir():
@@ -21,10 +24,41 @@ def get_dir():
 def get_hits():
     weight = flask.request.args.get('w')
     query = flask.request.args.get('q')
+
+    context = {"hits": []}
     queries = [word for word in query.split("+") if word not in stopwords]
+    counts = Counter(queries)
+    q_vec = []
+    s = set() # doc_ids that contain all words in queries
+    q_unique = lisst(counts.keys()) # unique words in queries
+    for word in q_unique:
+        if word in inverted_index:
+            s = s.intersection(inverted_index[word][1].keys())
+            if not s:
+                return flask.jsonify({"hits": []})
+            else:
+                tfq = counts[word]
+                idf = inverted_index[word][0]
+                q_vec.append(tfq*idf)
+        else:
+            return flask.jsonify({"hits": []})
 
+    squared_sum = sum([x**2 for x in q_vec])
+    norm_q = sqrt(squared_sum)
 
-
+    for doc in s:
+        d_vec = []
+        norm_d = None
+        for word in q_unique:
+            tfd = inverted_index[word][1][doc][0]
+            idf = inverted_index[word][0]
+            d_vec.append(tfd*idf)
+            if not norm_d:
+                norm_d_squared = inverted_index[word][1][doc][1]
+                norm_d = sqrt(norm_d_squared)
+        tfidf = np.dot(q_vec, d_vec) / (norm_q * norm_d)
+        weighted_score = weight * pagerank[doc] + (1 - weight) * tfidf
+        #TODO  add doc_id and score to hits, remember to sort by score desc, doc_id asc 
 
 
 def get_stopwords():
